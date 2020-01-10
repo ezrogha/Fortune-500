1454dbc69d94f9dd94e75e7448611e6168f11a64
Roghashin Timbiti <rtimbiti@gmail.com>
2020-01-10 09:33:02 +0300

    add script to create table if it doesn't exist

--- models/company.js
+++ models/company.js
@@ -13,10 +13,14 @@ module.exports = class Company {
     }
 
     save(cb) {
+        db.query('CREATE TABLE IF NOT EXISTS in_2019 (id serial PRIMARY KEY, ceo VARCHAR (100) NOT NULL, sector VARCHAR (100) NOT NULL, industry VARCHAR (100) NOT NULL, hq VARCHAR (100) NOT NULL, website VARCHAR (100) NOT NULL, employees integer NOT NULL, revenue integer NOT NULL)', (err, data) => {
+            
+        })
+        
         return db.query('INSERT INTO in_2019 (name, ceo, sector, industry, hq, website, employees, revenue) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [this.name, this.CEO, this.sector, this.industry, this.headquaters, this.website, this.employees, this.revenue], cb)
     }
 
     static fetchAll(cb) {
-        return db.query('SELECT * FROM in_2019', cb)
+        return db.query('SELECT * FROM inn_2019', cb)
     }
 }
